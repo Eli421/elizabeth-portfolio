5f82549f883e3772b2cd8bf8480caae9f52a3daa
Whanderley Fonseca Picado <whanderley.fonseca@globant.com>
2023-02-22 17:00:45 -0600

    001-create-file-storage Fixes some problems

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { AppRouter } from './router/AppRouter'
 
+import "./styles/base/index.scss"
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
