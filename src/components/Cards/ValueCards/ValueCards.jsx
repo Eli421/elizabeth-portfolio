88c7074252b5f27da00c8c2a780616a66fa676ee
Elizabeth Corrales <elizabethcorrales421@gmail.com>
2023-03-16 16:11:40 -0600

    004-value-component-completed
    Pending to add the .map functionality

---
+++ src/components/Cards/ValueCards/ValueCards.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { ValueCardsItem } from './ValueCardsItem'
+
+export const ValueCards = () => {
+  return (
+    <>
+        <ValueCardsItem/>
+    </>
+  )
+}
---
+++ src/components/Cards/ValueCards/ValueCardsItem.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import './ValueCardsItem.scss'
+import './ValueCards.scss'
+import Value_1 from '../../../assets/img/value_1.png'
+
+export const ValueCardsItem = () => {
+  return (
+    < >
+        <div className="values"> 
+            <div className="value">
+                <img className='value__image' src={Value_1} alt="first icon" />
+                <p className="value__text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos, reiciendis fugiat nihil cumque atque aspernatur explicabo </p>
+            </div>
+            <div className="value">
+                <img className='value__image' src={Value_1} alt="first icon" />
+                <p className="value__text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos, reiciendis fugiat nihil cumque atque aspernatur explicabo </p>
+            </div>
+            <div className="value">
+                <img className='value__image' src={Value_1} alt="first icon" />
+                <p className="value__text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos, reiciendis fugiat nihil cumque atque aspernatur explicabo </p>
+            </div>
+            <div className="value">
+                <img className='value__image' src={Value_1} alt="first icon" />
+                <p className="value__text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos, reiciendis fugiat nihil cumque atque aspernatur explicabo </p>
+            </div>
+        </div>
+    </>
+  )
+}
