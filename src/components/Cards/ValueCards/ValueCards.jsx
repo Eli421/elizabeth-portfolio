2ed1034031601d16a02e2df6f7d9468ab110d6c1
Elizabeth Corrales <elizabethcorrales421@gmail.com>
2023-03-20 11:50:22 -0600

    004-values-component-requiredChanges
    Modifications were made as requested

--- src/components/Cards/ValueCards/ValueCards.jsx
+++ src/components/Cards/ValueCards/ValueCards.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import { ValueCardsItem } from './ValueCardsItem'
+import React from 'react';
+import { ValueCardsItem } from './ValueCardsItem';
+import './ValueCards.scss'
 
 export const ValueCards = () => {
   return (
     <>
+      <div className="values">
         <ValueCardsItem/>
+        <ValueCardsItem/>
+        <ValueCardsItem/>
+        <ValueCardsItem/>
+      </div>
     </>
   )
 }
