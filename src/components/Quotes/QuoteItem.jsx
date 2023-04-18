0c0853b90e3cb97fdc03db5ff6b3ce5bd5f013cd
Elizabeth Corrales <elizabethcorrales421@gmail.com>
2023-04-18 15:51:40 -0600

    004-quotes-component-completed
    Component ready and functioning, it is still pending to adjust the size of the bg image in order to make it responsive

--- src/components/Quotes/QuoteItem.jsx
+++ src/components/Quotes/QuoteItem.jsx
@@ -1,13 +1,27 @@
 
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import '../Quotes/quoteItem.scss'
+import {quotes} from '../../data/quotes'
 
 
 export const QuoteItem = () => {
+
+  const [currentQuote, setCurrentQuote] = useState(quotes[0]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const currentIndex = quotes.indexOf(currentQuote);
+      const nextIndex = (currentIndex + 1) % quotes.length;
+
+      setCurrentQuote(quotes[nextIndex]);
+    }, 10000);
+
+      return( ) => clearInterval(interval);    
+  }, [currentQuote]);
+
   return (
     <div className="quoteItem">
-        <p className="quoteItem__text"> "Success is not final, failure is not fatal: it is the courage to continue that counts." </p>
-        <p className="quoteItem__author">- Winston Churchill</p>
+        <p className="quoteItem__text"> {currentQuote} </p>
     </div>
   )
 }
--- src/data/quotes.js
+++ src/data/quotes.js
@@ -1,44 +1,30 @@
 
 
 export const quotes = [
-    {   id: 1,
-        quote:'"Success is not final, failure is not fatal: it is the courage to continue that counts."',
-        author: '- Winston Churchill'
+    `"Success is not final, failure is not fatal: it is the courage to continue that counts." - Winston Churchill`,
 
-    },
-    {   id: 2,
-        quote:'"Success is liking yourself, liking what you do, and liking how you do it."',
-        author: '- Maya Angelou'
+   
+    `"Success is liking yourself, liking what you do, and liking how you do it." - Maya Angelou`,
 
-    },
-    {   id: 3,
-        quote:`"You miss 100% of the shots you don't take."`,
-        author: '- Wayne Gretzky'
+   
+    `"You miss 100% of the shots you don't take." - Wayne Gretzky`,
 
-    },
-    {   id: 4,
-        quote:`"You can't use up creativity. The more you use, the more you have." `,
-        author: '- Maya Angelou'
+   
+    `"You can't use up creativity. The more you use, the more you have." - Maya Angelou`,
 
-    },
-    {   id: 5,
-        quote:`"Believe you can and you're halfway there." `,
-        author: '- Theodore Roosevelt'
+   
+    `"Believe you can and you're halfway there." - Theodore Roosevelt`,
 
-    },
-    {   id: 6,
-        quote:"You can have it all. Just not all at once." ,
-        author: '-  Oprah Winfrey'
+   
+      
+    `"You can have it all. Just not all at once." - Oprah Winfrey`,
 
-    },
-    {   id: 7,
-        quote:"Artificial intelligence is a tool for humans, and it should be used to empower us to do great things.",
-        author: '- Fei-Fei Li'
+   
+      
+    `"Artificial intelligence is a tool for humans, and it should be used to empower us to do great things." - Fei-Fei Li`,
 
-    },
-    {   id: 8,
-        quote:"Coding is a new literacy and it is incumbent upon us to ensure that young women have access to this language." ,
-        author: '- Reshma Saujani'
+    
+    `"Coding is a new literacy and it is incumbent upon us to ensure that young women have access to this language". - Reshma Saujani`
 
-    },
+   
 ]
